1f115d3bb0eb3b0151aeb133dfa3bd5ac986b794
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-24 09:24:49 +0100

    Add 28.4 - TASK - WebSocket - ToDo List - Client View - control point

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
 class App extends React.Component {
+  render () {
+    return(
+      <div className='App'> 
+        <header>
+          <h1>* To-Do List Application *</h1>
+        </header>
 
-  render() {
-    return (
-      <div className="App">
-        Hello world
+        <section className='tasks-section' id='tasks-section'>
+          <h2>Tasks</h2>
+        </section>
+
+        <ul className='tasks-section__list' id='tasks-list'>
+          <li className='task'>Shopping <button className='btn btn--red'>Remove</button></li>
+          <li className='task'>Go out with a dog <button className='btn btn--red'>Remove</button></li>
+        </ul>
+
+        <form id='add-task-form'>
+          <input className='text-input' autoComplete='off' type='text' placeholder='Type your description' id='task-name' />
+          <button className='btn' type='submit'>Add</button>
+        </form>
       </div>
     );
   };
-
 };
 
 export default App;
