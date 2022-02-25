3900953ab511745d820e9040b7a1ed29170634f0
Marcin Nowak <maarcin.nowaak@gmail.com>
2022-02-25 17:27:27 +0100

    Add 28.4 - TASK - WebSocket - ToDo List - Unique ID

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
+const socket = require('socket.io');
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running...');
+  console.log('Server is running');
+});
+
+const io = socket(server);
+
+const tasks = [];
+
+app.get('*', (req, res) => {
+  res.send('Not found...');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user is connected');
 });
 
 app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
+  res.status(404).send({ message: 'error 404: not found...' })
 });
